633ebd7b0ef27ddd6581b5a2396a1c2d46ef7a6a
Ion Ivan <ivanion10@gmail.com>
2025-05-08 22:33:29 +0300

    solved bug in contry model

--- Modules/Translations/app/Models/Country.php
+++ Modules/Translations/app/Models/Country.php
@@ -5,7 +5,7 @@ namespace Modules\Translations\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Modules\Brokers\Models\Zone;
+use Modules\Translations\Models\Zone;
 
 class Country extends Model
 {
