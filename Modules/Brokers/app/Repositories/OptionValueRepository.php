868aff14223f3addace542feb4b1ebe4c30cc0eb
Ion Ivan <ivanion10@gmail.com>
2025-07-09 12:46:51 +0300

    Installed telescope for db queries debugging

--- Modules/Brokers/app/Repositories/OptionValueRepository.php
+++ Modules/Brokers/app/Repositories/OptionValueRepository.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
-class OptionValueRepository
+class OptionValueRepository 
 {
     protected OptionValue $model;
 
@@ -26,23 +26,23 @@ class OptionValueRepository
         //dd($language,$slug,$zoneCondition);
         OptionValue::with(["translations" => function (Builder $query) use ($language) {
             /** @var Illuminate\Contracts\Database\Eloquent\Builder   $query */
-            $query->where(...$language);
+             $query->where(...$language);
         }])->where("option_slug", "=", $slug)->where(function (Builder $query) use ($zoneCondition) {
             /** @var Illuminate\Contracts\Database\Eloquent\Builder   $query */
-
+           
             $query->where(...$zoneCondition)->orWhere('is_invariant', true);
         })
-
+         
             ->chunk(100, function ($options) use (&$results) {
                 $collection = new DynamicOptionValueCollection($options);
                 $list = [];
                 foreach ($collection->resolve() as $option) {
-
-                    //if the optionValue contain a link we will keep only the text
-                    preg_match('/<a[^>]*>(.*?)<\/a>/', $option["value"], $match);
+                
+                //if the optionValue contain a link we will keep only the text
+                preg_match('/<a[^>]*>(.*?)<\/a>/', $option["value"], $match);
                     $optionValue = ($match) ? $match[1] : $option["value"];
 
-                    //$items=explode(",",$option["value"]);
+                //$items=explode(",",$option["value"]);
                     $items = explode(",", $optionValue);
                     foreach ($items as $item) {
                         if (!array_key_exists(trim($item), $list) && trim($item) !== "")
@@ -50,9 +50,9 @@ class OptionValueRepository
                     }
                 }
                 $results = array_merge($results, $list);
-            });
+         });
 
-        return array_unique($results);
+         return array_unique($results);
     }
 
     // ===== NEW ADDED METHODS FOR CRUD OPERATIONS =====
