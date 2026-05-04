68adf8a5c8372d0b7e484d4dbe9b1321421281e0
Ion Ivan <ivanion10@gmail.com>
2026-05-04 13:09:19 +0300

    Created broker groups API

--- Modules/Brokers/app/Forms/BrokerGroupForm.php
+++ Modules/Brokers/app/Forms/BrokerGroupForm.php
@@ -27,8 +27,8 @@ class BrokerGroupForm extends Form
 
                         'is_active' => Field::select('Is Active', $this->booleanOptions(), ['required' => true]),
 
-                        'brokers' => Field::multiselect('Brokers', '/broker-groups/search-by-broker-trading-name', 'trading_name', null, ['required' => true]),
-                        //'brokers' => Field::multiselect('Brokers', null, null, $this->getBrokersList(), ['required' => true]),
+                        //'brokers' => Field::multiselect('Brokers', '/broker-groups/search-by-broker-trading-name', 'trading_name', null, ['required' => true]),
+                        'brokers' => Field::multiselect('Brokers', null, null, $this->getBrokersList(), ['required' => true]),
                     ],
                 ],
             ],
--- Modules/Brokers/app/Http/Controllers/BrokerController.php
+++ Modules/Brokers/app/Http/Controllers/BrokerController.php
@@ -37,7 +37,6 @@ use Modules\Brokers\Enums\BrokerType as BrokerTypeEnum;
 
 class BrokerController extends Controller
 {
-
     public function __construct(
         protected BrokerService $brokerService,
         private readonly BrokerTableConfig $tableConfig,
@@ -56,7 +55,7 @@ class BrokerController extends Controller
         //tested with http://localhost:8000/api/v1/brokers?language[eq]=ro&page=1&columns[in]=trading_name,trading_fees,account_type,jurisdictions,promotion_title,fixed_spreads,support_options&order_by[eq]=+account_type
     }
 
-     /**
+    /**
      * OK
      * Register a new broker
      */
@@ -66,45 +65,59 @@ class BrokerController extends Controller
             // Create the broker
             $broker = DB::transaction(function () use ($request) {
                 $data = $request->validated();
-                $broker = $this->brokerService->registerBroker($data['trading_name'], $data['broker_type_id'], $data['country_id']);
+                $broker = $this->brokerService->registerBroker(
+                    $data["trading_name"],
+                    $data["broker_type_id"],
+                    $data["country_id"],
+                );
                 //create a default team for the broker and add a user in that team
                 $team = $this->teamService->createTeam([
-                    'broker_id' => $broker->id,
-                    'name' => 'Default Team',
-                    'description' => 'Default team for the broker',
-                    'permissions' => [],
+                    "broker_id" => $broker->id,
+                    "name" => "Default Team",
+                    "description" => "Default team for the broker",
+                    "permissions" => [],
                 ]);
                 $user = $this->teamService->createTeamUser([
-                    'broker_team_id' => $team->id,
-                    'name' => 'Broker Admin',
-                    'email' => $data['email'],
-                    'is_active' => true,
+                    "broker_team_id" => $team->id,
+                    "name" => "Broker Admin",
+                    "email" => $data["email"],
+                    "is_active" => true,
                 ]);
 
                 //generate user permission for the user
-                $this->permissionService->createPermission([
-                    'subject_type' => BrokerTeamUser::class,
-                    'subject_id' => $user->id,
-                    'resource_id' => $broker->id,
-                    'resource_value' => $data['trading_name'],
-                ],AuthPermission::BROKER,AuthAction::MANAGE);
+                $this->permissionService->createPermission(
+                    [
+                        "subject_type" => BrokerTeamUser::class,
+                        "subject_id" => $user->id,
+                        "resource_id" => $broker->id,
+                        "resource_value" => $data["trading_name"],
+                    ],
+                    AuthPermission::BROKER,
+                    AuthAction::MANAGE,
+                );
 
                 //load default challenge categories for the broker
-                if($broker->brokerType->name == BrokerTypeEnum::PROP_FIRM->value) {
-                    $this->challengeService->cloneDefaultChallengesToBroker($broker->id);
+                if (
+                    $broker->brokerType->name ==
+                    BrokerTypeEnum::PROP_FIRM->value
+                ) {
+                    $this->challengeService->cloneDefaultChallengesToBroker(
+                        $broker->id,
+                    );
                 }
-
 
                 //generate magic link for broker
                 $magicLink = $this->magicLinkService->generateForTeamUser(
                     $user,
-                    'registration',
-                    ['requested_at' => now()],
-                    96 // 96 hours = 4 days
+                    "registration",
+                    ["requested_at" => now()],
+                    96, // 96 hours = 4 days
                 );
 
                 //send email with magic link
-               $this->mailService->to($user->email)->send(new MagicLinkMail($magicLink));
+                $this->mailService
+                    ->to($user->email)
+                    ->send(new MagicLinkMail($magicLink));
 
                 return $broker;
             });
@@ -112,26 +125,30 @@ class BrokerController extends Controller
             // Load the broker type relationship
             //$broker->load('brokerType', 'dynamicOptionsValues');
 
-            return response()->json([
-                'success' => true,
-                'message' => 'Broker registered successfully',
-                'data' => [
-                    'broker' => $broker,
-                    'broker_type' => $broker->brokerType->name
-                ]
-            ], 201);
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Broker registered successfully",
+                    "data" => [
+                        "broker" => $broker,
+                        "broker_type" => $broker->brokerType->name,
+                    ],
+                ],
+                201,
+            );
         } catch (\Exception $e) {
-            Log::error('Broker registration failed: ' . $e->getMessage());
+            Log::error("Broker registration failed: " . $e->getMessage());
 
-            return response()->json([
-                'success' => false,
-                'message' => 'Failed to register broker',
-                'error' => $e->getMessage()
-            ], 500);
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Failed to register broker",
+                    "error" => $e->getMessage(),
+                ],
+                500,
+            );
         }
     }
-
-
 
     /**
      * @OA\Get(
@@ -149,7 +166,7 @@ class BrokerController extends Controller
      *         response=200,
      *         description="Successful operation"
      *     ),
-     * 
+     *
      *     @OA\Response(
      *         response=422,
      *         description="These request do not match our records"
@@ -158,57 +175,57 @@ class BrokerController extends Controller
      */
     public function show($id, Request $request)
     {
-
         $validator = Validator::make($request->query(), [
-            'language.eq' => 'required|string',
-            'country.eq' => 'required|string',
-            'tab.eq' => 'nullable|string|in:reviews,default',
+            "language.eq" => "required|string",
+            "country.eq" => "required|string",
+            "tab.eq" => "nullable|string|in:reviews,default",
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
+            return response()->json(["errors" => $validator->errors()], 422);
         }
         $brokerData = Broker::findOrFail($id);
         // Retrieve query parameters
-        $lang = $request->query('language')['eq']; // Fetch `lang[eq]`
-        $country = $request->query('country')['eq'];
-        $tab = $request->query('tab')['eq'] ?? "all";
+        $lang = $request->query("language")["eq"]; // Fetch `lang[eq]`
+        $country = $request->query("country")["eq"];
+        $tab = $request->query("tab")["eq"] ?? "all";
 
-        $zone = Zone::where("countries", "like", "%$country%")->first()->zone_code;
+        $zone = Zone::where("countries", "like", "%$country%")->first()
+            ->zone_code;
         if ($tab == "all") {
             //return all options
         } else {
-            $setting = Setting::where('key', "tab_" . $tab)->firstOrFail();
+            $setting = Setting::where("key", "tab_" . $tab)->firstOrFail();
             //return options of a specific tab
             $tabData = json_decode($setting->value, 1);
-            if (!empty($tabData['options'])) {
-                $options = $tabData['options'];
+            if (!empty($tabData["options"])) {
+                $options = $tabData["options"];
 
                 // If 'relations' is absent, set to null, otherwise use the value
-                $relations = $tabData['relations'] ?? null;
+                $relations = $tabData["relations"] ?? null;
 
                 //dd($options,$relations);
-
             } else {
-                return response()->json([
-                    'error' => "The required keys 'options' or 'relations' are missing in the settings tab for key={$tab}.",
-                ], 422);
+                return response()->json(
+                    [
+                        "error" => "The required keys 'options' or 'relations' are missing in the settings tab for key={$tab}.",
+                    ],
+                    422,
+                );
             }
         }
 
-
         $additionalInfo = [
-            'language' => $lang ?? 'default',
-            'zone' => $zone ?? 'default',
-            'tab' => $tab ?? 'default',
+            "language" => $lang ?? "default",
+            "zone" => $zone ?? "default",
+            "tab" => $tab ?? "default",
         ];
 
         return response()->json([
-            'broker' => $brokerData,
-            'additional_info' => $additionalInfo,
+            "broker" => $brokerData,
+            "additional_info" => $additionalInfo,
         ]);
     }
-
 
     /**
      * get broker context
@@ -218,15 +235,19 @@ class BrokerController extends Controller
      */
     public function getBrokerInfo(Request $request, $id)
     {
-        
         try {
-            return response()->json($this->brokerService->getBrokerContext($id));
+            return response()->json(
+                $this->brokerService->getBrokerContext($id),
+            );
         } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Failed to get broker context',
-                'error' => $e->getMessage(),
-            ], 500);
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Failed to get broker context",
+                    "error" => $e->getMessage(),
+                ],
+                500,
+            );
         }
     }
 
@@ -236,18 +257,23 @@ class BrokerController extends Controller
      */
     public function getFormConfig()
     {
-       
         try {
-            return response()->json([
-                'success' => true,
-                'data' => $this->formConfig->getFormData()
-            ], 200);
+            return response()->json(
+                [
+                    "success" => true,
+                    "data" => $this->formConfig->getFormData(),
+                ],
+                200,
+            );
         } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Error getting form data',
-                'error' => $e->getMessage()
-            ], 500);
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error getting form data",
+                    "error" => $e->getMessage(),
+                ],
+                500,
+            );
         }
     }
 
@@ -258,17 +284,19 @@ class BrokerController extends Controller
      * @param BrokerListRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function getBrokerList(BrokerListRequest $request, ?int $zone_id, ?int $country_id=null)
-    {
-      //TO DO 1: Get table filters as a function of logged user permissions
-      //If the user is a platform user he should see only brokers that he has access to
-      //TO DO 2: Add zone_id and country_id to filters
-     // $filters['zone_id'] = $zone_id;
-     // $filters['country_id'] = $country_id;
+    public function getBrokerList(
+        BrokerListRequest $request,
+        ?int $zone_id,
+        ?int $country_id = null,
+    ) {
+        //TO DO 1: Get table filters as a function of logged user permissions
+        //If the user is a platform user he should see only brokers that he has access to
+        //TO DO 2: Add zone_id and country_id to filters
+        // $filters['zone_id'] = $zone_id;
+        // $filters['country_id'] = $country_id;
         try {
-
             $filters = $request->getFilters();
-           
+
             $orderBy = $request->getOrderBy();
             $orderDirection = $request->getOrderDirection();
             $perPage = $request->getPerPage();
@@ -286,32 +314,39 @@ class BrokerController extends Controller
             //     'is_active' => $validated['is_active'] ?? null,
             // ];
 
-            $brokers = $this->brokerService->getBrokerList($perPage, $orderBy, $orderDirection, $filters);
+            $brokers = $this->brokerService->getBrokerList(
+                $perPage,
+                $orderBy,
+                $orderDirection,
+                $filters,
+            );
 
             return response()->json([
-                'success' => true,
-                'data' => BrokerListResource::collection($brokers->items()),
-                'table_columns_config' => $this->tableConfig->columns(),
-                'filters_config' => $this->tableConfig->filters(),
-                'form_config' => $this->formConfig->getFormData(),
-                'pagination' => [
-                    'current_page' => $brokers->currentPage(),
-                    'last_page' => $brokers->lastPage(),
-                    'per_page' => $brokers->perPage(),
-                    'total' => $brokers->total(),
-                    'from' => $brokers->firstItem(),
-                    'to' => $brokers->lastItem(),
-                ]
+                "success" => true,
+                "data" => BrokerListResource::collection($brokers->items()),
+                "table_columns_config" => $this->tableConfig->columns(),
+                "filters_config" => $this->tableConfig->filters(),
+                "form_config" => $this->formConfig->getFormData(),
+                "pagination" => [
+                    "current_page" => $brokers->currentPage(),
+                    "last_page" => $brokers->lastPage(),
+                    "per_page" => $brokers->perPage(),
+                    "total" => $brokers->total(),
+                    "from" => $brokers->firstItem(),
+                    "to" => $brokers->lastItem(),
+                ],
             ]);
         } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Failed to get broker list',
-                'error' => $e->getMessage(),
-            ], 500);
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Failed to get broker list",
+                    "error" => $e->getMessage(),
+                ],
+                500,
+            );
         }
     }
-
 
     /**
      * Toggle active status of a broker
@@ -319,17 +354,23 @@ class BrokerController extends Controller
      * @param Broker $broker
      * @return \Illuminate\Http\JsonResponse
      */
-    public function toggleActiveStatus(BrokerToggleActiveRequest $request, Broker $broker)
-    {
-       
+    public function toggleActiveStatus(
+        BrokerToggleActiveRequest $request,
+        Broker $broker,
+    ) {
         try {
-            return response()->json($this->brokerService->toggleActiveStatus($broker->id));
+            return response()->json(
+                $this->brokerService->toggleActiveStatus($broker->id),
+            );
         } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Failed to toggle active status',
-                'error' => $e->getMessage(),
-            ], 500);
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Failed to toggle active status",
+                    "error" => $e->getMessage(),
+                ],
+                500,
+            );
         }
     }
 
@@ -339,20 +380,24 @@ class BrokerController extends Controller
             $countries = Country::all();
             $brokerTypes = BrokerType::all();
             return response()->json([
-                'success' => true,
-                'message' => 'Broker types and countries fetched successfully',
-                'countries' => new CountryCollection($countries, ['minimal' => true]),
-                'brokerTypes' => BrokerTypeResource::collection($brokerTypes),
+                "success" => true,
+                "message" => "Broker types and countries fetched successfully",
+                "countries" => new CountryCollection($countries, [
+                    "minimal" => true,
+                ]),
+                "brokerTypes" => BrokerTypeResource::collection($brokerTypes),
             ]);
         } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Failed to get broker types and countries',
-                'error' => $e->getMessage(),
-            ], 500);
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Failed to get broker types and countries",
+                    "error" => $e->getMessage(),
+                ],
+                500,
+            );
         }
     }
-
 
     /**
      * Sanitize input for LIKE queries by escaping special characters
@@ -362,6 +407,6 @@ class BrokerController extends Controller
     private function sanitizeLikeInput(string $input): string
     {
         // Escape special LIKE characters: %, _, \
-        return str_replace(['\\', '%', '_'], ['\\\\', '\%', '\_'], $input);
+        return str_replace(["\\", "%", "_"], ["\\\\", "\%", "\_"], $input);
     }
 }
